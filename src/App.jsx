bad1a8f1bc677a0f9514b6e9f5bf568354a7bdd4
Swapnil Katwe <swapnil.katwe111@gmail.com>
2024-12-19 14:41:39 +0100

    Issue solved: If Modal is closed using ESC keyboard button, Modal will not appear after that. Solution: Use of onClose buildin prop of dialog and manage from app.

--- src/App.jsx
+++ src/App.jsx
@@ -80,7 +80,7 @@ function App() {
 
   return (
     <>
-      <Modal open={modalIsOpen}>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
