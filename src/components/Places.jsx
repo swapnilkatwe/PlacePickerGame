e97ee385157397ab3df5d3da1ed97459debb2ce5
Swapnil Katwe <swapnil.katwe111@gmail.com>
2024-12-30 14:14:06 +0100

    Issue fix: place obj should be pass on onSelectPlace as per latest logic.

--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -9,7 +9,7 @@ export default function Places({ title, places, isLoading, loadingText, fallback
         <ul className="places">
           {places.map((place) => (
             <li key={place.id} className="place-item">
-              <button onClick={() => onSelectPlace(place.id)}>
+              <button onClick={() => onSelectPlace(place)}>
               <img src={`http://localhost:3000/${place.image.src}`} alt={place.image.alt} />
               <h3>{place.title}</h3>
               </button>
