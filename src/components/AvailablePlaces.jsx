6017236bdeef0b117f2da1215fbb1a43091e8a10
Swapnil Katwe <swapnil.katwe111@gmail.com>
2024-12-30 13:47:21 +0100

    Fetching Places from places api and loading data and images asynchronously. Error handling and added ErrorScreen component to show error.

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,12 +1,63 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Places from "./Places";
+import ErrorScreen from "./ErrorScreen";
 
+// ASYNC AWAIT CAN NOT BE USE ON THE COMPONENT FUNCTION, THIS IS A RESTRICTION FROM REACT ITSELF
 export default function AvailablePlaces({ onSelectPlace }) {
+
+    const [isFetching, setIsFetching] = useState(false);
+    const [availablePlaces, setAvailablePlaces] = useState([]);
+    const [error, setError] = useState();
+
+    const usingAsync = true;
+
+    if (usingAsync) {
+        // USING ASYNC AWAIT BY LITTLE TRICK AS BWLOW:
+        useEffect(() => {
+            async function fetchPlaces() {
+                setIsFetching(true);
+                try {
+                    const response = await fetch("http://localhost:3000/places");
+                    const responseData = await response.json();
+                    console.log(responseData);
+    
+                    if(!response.ok) {
+                        throw new Error("Failed to fetch places");
+                    }
+                    setAvailablePlaces(responseData.places);
+
+                } catch (error) {
+                    setError(error);
+                }
+                setIsFetching(false);
+            }
+
+            fetchPlaces();
+        }, []);
+
+        if (error) {
+           return <ErrorScreen title="Error Occured!" message={error.message}/>
+        }
+    } else {
+        // USING TRADITIONAL WAY OF CALLING API
+        useEffect(() => {
+            setIsFetching(true);
+            fetch("http://localhost:3000/places").then((response) => {
+                return response.json();
+            }).then((responseData) => {
+                setAvailablePlaces(responseData.places);
+                setIsFetching(false);
+            })
+        }, []);
+    }
+
     return (
         <Places
             title="Available Places"
-            places={[]}
-            fallbackText="Sorting places by distance..."
+            places={availablePlaces}
+            isLoading = {isFetching}
+            loadingText = "Data is loading..."
+            fallbackText="No places available..."
             onSelectPlace={onSelectPlace}
         />);
 }
