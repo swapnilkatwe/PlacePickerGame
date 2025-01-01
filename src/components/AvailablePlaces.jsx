bbfdb7684cd646d96e8d80f179b525e754e79a75
Swapnil Katwe <swapnil.katwe111@gmail.com>
2025-01-01 16:25:15 +0100

    useFetch Custom hook integrated with AvailablePlaces. Geolocation code is not integrated in this commit.

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,57 +3,28 @@ import Places from "./Places";
 import ErrorScreen from "./ErrorScreen";
 import { sortPlacesByDistance } from "../loc";
 import { fetchAvailablePlaces } from "../http.js";
+import { useFetch } from "../customHooks/useFetch.js";
 
-// ASYNC AWAIT CAN NOT BE USE ON THE COMPONENT FUNCTION, THIS IS A RESTRICTION FROM REACT ITSELF
+// navigator.geolocation.getCurrentPosition((position) => {
+//     const sortedPlaces = sortPlacesByDistance(
+//         places,
+//         position.coords.latitude,
+//         position.coords.longitude)
+
+//     setAvailablePlaces(sortedPlaces);
+// });
+
 export default function AvailablePlaces({ onSelectPlace }) {
 
-    const [isFetching, setIsFetching] = useState(false);
-    const [availablePlaces, setAvailablePlaces] = useState([]);
-    const [error, setError] = useState();
+    const {
+        isFetching,
+        fetchedData: availablePlaces,
+        setFetchedData: setAvailablePlaces,
+        error
+    } = useFetch(fetchAvailablePlaces, []);
 
-    const usingAsync = true;
-
-    if (usingAsync) {
-        // USING ASYNC AWAIT BY LITTLE TRICK AS BWLOW:
-        useEffect(() => {
-            async function fetchPlaces() {
-                setIsFetching(true);
-                try {
-                    // Fetching places
-                    const places = await fetchAvailablePlaces();
-
-                    navigator.geolocation.getCurrentPosition((position) => {
-                        const sortedPlaces = sortPlacesByDistance(
-                            places,
-                            position.coords.latitude,
-                            position.coords.longitude)
-
-                            setAvailablePlaces(sortedPlaces);
-                            setIsFetching(false);
-                    });
-                } catch (error) {
-                    setError(error);
-                    setIsFetching(false);
-                }
-            }
-
-            fetchPlaces();
-        }, []);
-
-        if (error) {
-            return <ErrorScreen title="Error Occured!" message={error.message} />
-        }
-    } else {
-        // USING TRADITIONAL WAY OF CALLING API
-        useEffect(() => {
-            setIsFetching(true);
-            fetch("http://localhost:3000/places").then((response) => {
-                return response.json();
-            }).then((responseData) => {
-                setAvailablePlaces(responseData.places);
-                setIsFetching(false);
-            })
-        }, []);
+    if (error) {
+        return <ErrorScreen title="Error Occured!" message={error.message} />
     }
 
     return (
@@ -66,3 +37,16 @@ export default function AvailablePlaces({ onSelectPlace }) {
             onSelectPlace={onSelectPlace}
         />);
 }
+
+
+
+// ---> USING TRADITIONAL WAY OF CALLING API
+// useEffect(() => {
+//     setIsFetching(true);
+//     fetch("http://localhost:3000/places").then((response) => {
+//         return response.json();
+//     }).then((responseData) => {
+//         setAvailablePlaces(responseData.places);
+//         setIsFetching(false);
+//     })
+// }, []);
